961c09d06ce04c2a94c61110a84b0ca91825f36f
Almendra-Isabel <almensull@gmail.com>
2024-11-21 10:44:58 -0500

    Cambio desde la rama Almendra

--- crear.php
+++ crear.php
@@ -13,23 +13,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $libro->registrarLibro();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Registrar Libro</title>
-</head>
-<body>
-    <h1>Registrar Libro</h1>
-    <form method="POST">
-        <input type="text" name="titulo" placeholder="Titulo" required>
-        <input type="text" name="autor" placeholder="Autor" required>
-        <input type="text" name="editorial" placeholder="Editorial" required>
-        <input type="number" name="año" placeholder="Año" required>
-        <input type="text" name="genero" placeholder="Genero" required>
-        <button type="submit">Registrar</button>
-        <a href="index.php">volver</a>
-    </form>
-</body>
-</html>
